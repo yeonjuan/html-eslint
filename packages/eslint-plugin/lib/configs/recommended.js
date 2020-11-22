88cb97332882fe8705ee4162c7a882194c7c9b36
yeonjuan <yeonjuan93@naver.com>
2020-11-23 00:58:40 +0900

    fix rule name

--- packages/eslint-plugin/lib/configs/recommended.js
+++ packages/eslint-plugin/lib/configs/recommended.js
@@ -1,7 +1,7 @@
 module.exports = {
   rules: {
     "@html-eslint/require-lang": "error",
-    "@html-eslint/equire-img-alt": "error",
+    "@html-eslint/require-img-alt": "error",
     "@html-eslint/require-doctype": "error",
     "@html-eslint/require-title": "error",
   },
