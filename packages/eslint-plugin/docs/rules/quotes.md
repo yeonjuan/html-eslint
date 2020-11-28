def665d635396b98d99bdb994b2dab22a273cac7
YeonJuan <yeonjuan93@naver.com>
2020-11-28 17:54:38 +0900

    add `quotes` rule (#9)

---
+++ packages/eslint-plugin/docs/rules/quotes.md
@@ -0,0 +1,54 @@
+# Enforce consistent quoting attributes with double(`"`) or single(`'`)
+
+## Rule Details
+
+This rule enforces the consistent use of double(`"`) or single(`'`) qoutes for element attributes.
+
+## Options
+
+This rule has two options
+
+- `"double"` (default): requires the use of double quotes(`"`).
+- `"single"`: requires the use of single quotes(`'`)
+
+### "double"
+
+Examples of **incorrect** code for this rule with the default `"double"` option:
+
+<!-- prettier-ignore-start -->
+```html
+<div id='foo'> </div>
+<div id=foo> </div>
+```
+<!-- prettier-ignore-end -->
+
+Examples of **correct** code for this rule with the default `"double"` option:
+
+<!-- prettier-ignore-start -->
+```html
+<div id="foo"> </div>
+```
+<!-- prettier-ignore-end -->
+
+### "single"
+
+Examples of **incorrect** code for this rule with the `"single"` option:
+
+<!-- prettier-ignore-start -->
+```html
+<div id="foo"> </div>
+<div id=foo> </div>
+```
+<!-- prettier-ignore-end -->
+
+Examples of **correct** code for this rule with the default `"single"` option:
+
+<!-- prettier-ignore-start -->
+```html
+<div id='foo'> </div>
+```
+<!-- prettier-ignore-end -->
+
+## Further reading
+
+[MDN - Quoting attributes](https://developer.mozilla.org/en-US/docs/MDN/Guidelines/Code_guidelines/HTML#Quoting_attributes)
---
+++ packages/eslint-plugin/tests/rules/quotes.test.js
@@ -0,0 +1,115 @@
+const createRuleTester = require("../rule-tester");
+const rule = require("../../lib/rules/quotes");
+
+const ruleTester = createRuleTester();
+
+ruleTester.run("quotes", rule, {
+  valid: [
+    {
+      code: `<div id = " foo ">`,
+    },
+    {
+      code: `<div id = "foo ">`,
+    },
+    {
+      code: `<div id = " foo">`,
+    },
+    {
+      code: `
+<div id="
+foo">
+`,
+    },
+    {
+      code: `<div id="foo">`,
+    },
+
+    {
+      code: `<div id="">`,
+    },
+
+    {
+      code: `<div id>`,
+    },
+    {
+      code: `<div aa>`,
+    },
+    {
+      code: `<div id='foo'>`,
+      options: ["single"],
+    },
+  ],
+  invalid: [
+    {
+      code: `<div id='foo'>`,
+      output: `<div id="foo">`,
+      errors: [
+        {
+          messageId: "unexpected",
+        },
+      ],
+    },
+    {
+      code: `<div id='
+foo'>`,
+      output: `<div id="
+foo">`,
+      errors: [
+        {
+          messageId: "unexpected",
+        },
+      ],
+    },
+    {
+      code: `<div id='foo' style='bar'>`,
+      output: `<div id="foo" style="bar">`,
+      errors: [
+        {
+          messageId: "unexpected",
+        },
+        {
+          messageId: "unexpected",
+        },
+      ],
+    },
+    {
+      code: `<div id="foo">`,
+      output: `<div id='foo'>`,
+      options: ["single"],
+      errors: [
+        {
+          messageId: "unexpected",
+        },
+      ],
+    },
+    {
+      code: `<div id=foo>`,
+      output: `<div id="foo">`,
+      errors: [
+        {
+          messageId: "missing",
+        },
+      ],
+    },
+    {
+      code: `<div id=
+foo>`,
+      output: `<div id=
+"foo">`,
+      errors: [
+        {
+          messageId: "missing",
+        },
+      ],
+    },
+    {
+      code: `<div id=  foo>`,
+      output: `<div id=  "foo">`,
+      errors: [
+        {
+          messageId: "missing",
+        },
+      ],
+    },
+  ],
+});
