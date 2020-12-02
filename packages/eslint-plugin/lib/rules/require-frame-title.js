294a48a5de0b1f0732300ed052bea4dae6f2ddbf
YeonJuan <yeonjuan93@naver.com>
2020-12-02 20:32:28 +0900

    add `require-frame-title` rule (#23)

---
+++ packages/eslint-plugin/lib/rules/require-frame-title.js
@@ -0,0 +1,45 @@
+const { RULE_CATEGORY } = require("../constants");
+const { NodeUtils } = require("./utils");
+
+const MESSAGE_IDS = {
+  MISSING: "missing",
+  UNEXPECTED: "unexpected",
+};
+
+module.exports = {
+  meta: {
+    type: "code",
+
+    docs: {
+      description: "Require `title` in `<frame>`, `<iframe>`",
+      category: RULE_CATEGORY.ACCESSIBILITY,
+      recommended: false,
+    },
+
+    fixable: false,
+    schema: [],
+    messages: {
+      [MESSAGE_IDS.MISSING]: "Missing `title` attribute in {{frame}}",
+      [MESSAGE_IDS.UNEXPECTED]: "Unexpected empty `title`.",
+    },
+  },
+
+  create(context) {
+    return {
+      "Frame, Iframe"(node) {
+        const title = NodeUtils.findAttr(node, "title");
+        if (!title) {
+          context.report({
+            node: node.startTag,
+            messageId: MESSAGE_IDS.MISSING,
+          });
+        } else if (title.value.trim().length === 0) {
+          context.report({
+            node: title,
+            messageId: MESSAGE_IDS.UNEXPECTED,
+          });
+        }
+      },
+    };
+  },
+};
