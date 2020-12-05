2f37faa3cb968156bf230a4193048822fcb5fd6f
YeonJuan <yeonjuan93@naver.com>
2020-12-05 23:06:22 +0900

    add `require-meta-viewport` rule (#26)

---
+++ packages/eslint-plugin/tests/rules/require-meta-viewport.test.js
@@ -0,0 +1,62 @@
+const createRuleTester = require("../rule-tester");
+const rule = require("../../lib/rules/require-meta-viewport");
+
+const ruleTester = createRuleTester();
+
+ruleTester.run("require-meta-viewport", rule, {
+  valid: [
+    {
+      code: `
+<html>
+      <head>
+          <meta name="viewport" content="foo">
+      </head>
+</html>
+`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+<html>
+      <head>
+          <meta name="description">
+      </head>
+</html>
+`,
+
+      errors: [
+        {
+          messageId: "missing",
+        },
+      ],
+    },
+    {
+      code: `
+<html>
+      <head>
+      </head>
+</html>
+`,
+      errors: [
+        {
+          messageId: "missing",
+        },
+      ],
+    },
+    {
+      code: `
+<html>
+      <head>
+          <meta name="viewport" content="">
+      </head>
+</html>
+`,
+      errors: [
+        {
+          messageId: "empty",
+        },
+      ],
+    },
+  ],
+});
