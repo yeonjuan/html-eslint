a0362e95e0eb29e65e7e1bdf671292257b291f42
yeonjuan <yeonjuan93@naver.com>
2020-12-06 23:34:36 +0900

    setup eslint

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: 9,
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
-    }
+    },
   },
   env: {
     node: true,
