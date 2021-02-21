fa3e8a94c8a54631c39dfc47273b884f95529a08
yeonjuan <yeonjuan93@naver.com>
2021-02-21 16:22:18 +0900

    draft: no-target-blank

---
+++ packages/eslint-plugin/lib/rules/no-target-blank.js
@@ -0,0 +1,35 @@
+const { RULE_CATEGORY } = require("../constants");
+const { NodeUtils } = require("./utils");
+
+const MESSAGE_IDS = {
+  UNEXPECTED: "unexpected",
+};
+
+module.exports = {
+  meta: {
+    type: "code",
+
+    docs: {
+      description: "Disallow usage of unsafe `target='_blank'`",
+      category: RULE_CATEGORY.BEST_PRACTICE,
+      recommended: false,
+    },
+
+    fixable: null,
+    schema: [],
+    messages: {
+      [MESSAGE_IDS.UNEXPECTED]: "Unexpected ...", // TODO Message
+    },
+  },
+
+  create(context) {
+    return {
+      A(node) {
+        const target = NodeUtils.findAttr(node, "target");
+        const href = NodeUtils.findAttr(node, 'href');
+        const rel = NodeUtils.findAttr(node, 'rel');
+        // TODO: report
+      },
+    };
+  },
+};
---
+++ packages/eslint-plugin/tests/rules/no-target-blank.test.js
@@ -0,0 +1,28 @@
+const createRuleTester = require("../rule-tester");
+const rule = require("../../lib/rules/no-target-blank");
+
+const ruleTester = createRuleTester();
+
+ruleTester.run("no-target-blank", rule, {
+  valid: [
+    {
+      code: `<a target='_blank' rel='noreferrer' href='http://www.foo.com'></a>`
+    },
+    {
+      code: `<a target='_blank' rel='noreferrer noopener' href='http://www.foo.com'></a>`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+<a target='_blank' href='http://www.foo.com'></a>
+`,
+
+      errors: [
+        {
+          message: "Unexpected ...",
+        },
+      ],
+    },
+  ],
+});
