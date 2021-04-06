8bd31041458308dca188c825239d354d0ebd55ef
yeonjuan <yeonjuan93@naver.com>
2021-04-06 12:38:15 +0900

    rollback test

--- packages/parser/tests/parser.test.js
+++ packages/parser/tests/parser.test.js
@@ -1,15 +1,81 @@
 "use strict";
 
-const parser = require("../lib");
+const parserForEslint = require("../lib");
 
 describe("parser", () => {
-  it("duplicate attributes", () => {
+
+  const parser = {
+    parse (code) {
+      return parserForEslint.parseForESLint(code).ast;
+    }
+  }
+
+  it("parse", () => {
+    const code = "<html></html>";
+    expect(() => parser.parse(code)).not.toThrow();
+  });
+
+  it("base", () => {
     const code = `
-<html>
-  <body>
-    <img src="./image.png">
-  </body>
-</html>`;
-    expect(parser.parseForESLint(code).ast).toMatchSnapshot();
+  <html>
+  </html>`;
+    expect(parser.parse(code)).toMatchSnapshot();
+  });
+
+  it("DOCTYPE", () => {
+    const code = `
+  <!DOCTYPE HTML>
+  <html>
+  </html>`;
+    expect(parser.parse(code)).toMatchSnapshot();
+  });
+
+  it("attr", () => {
+    const code = `
+<div attr="foo">
+</div>
+`;
+    expect(parser.parse(code)).toMatchSnapshot();
+  });
+
+  it("comment", () => {
+    const code = `
+<div>
+<!-- comment -->
+</div>
+`;
+    expect(parser.parse(code)).toMatchSnapshot();
+  });
+
+  it("comment multiline", () => {
+    const code = `
+<!--
+  comment
+-->
+`;
+    expect(parser.parse(code)).toMatchSnapshot();
+  });
+
+  it("no opening tag", () => {
+    const code = `<!DOCTYPE html>
+    <html lang="en">
+    </head>
+    <body>
+    
+      <table>
+        <tr>
+          <td>Table 1</td>
+        </tr>
+      </table>
+    
+      <table>
+        <tr>
+          <td>Table 2</td>
+        </tr>
+      </table>
+    
+    </body>
+    </html>`;
+    expect(parser.parse(code)).toMatchSnapshot();
   });
 });
