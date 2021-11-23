f7ad9ce149c7c5d3bb3945ff2973b2d8e25c7630
YeonJuan <yeonjuan93@naver.com>
2021-11-24 01:26:21 +0900

    fix: [parser] use case-insensitive comparison on tag name (#82)

--- packages/eslint-plugin/tests/rules/require-closing-tags.test.js
+++ packages/eslint-plugin/tests/rules/require-closing-tags.test.js
@@ -11,6 +11,24 @@ ruleTester.run("require-closing-tags", rule, {
     {
       code: `<img>`,
     },
+    // https://github.com/yeonjuan/html-eslint/issues/73
+    {
+      code: `
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+<defs>
+<linearGradient id="myGradient" gradientTransform="rotate(90)">
+</linearGradient>
+</defs>
+</svg>
+</body>
+</html>
+`,
+    },
   ],
   invalid: [
     {
@@ -46,6 +64,27 @@ ruleTester.run("require-closing-tags", rule, {
       errors: [
         {
           messageId: "unexpected",
+        },
+      ],
+    },
+    {
+      code: `
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+<defs>
+<linearGradient id="myGradient" gradientTransform="rotate(90)">
+</defs>
+</svg>
+</body>
+</html>
+`,
+      errors: [
+        {
+          messageId: "missing",
         },
       ],
     },
