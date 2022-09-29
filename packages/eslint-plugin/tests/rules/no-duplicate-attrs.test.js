87eaacfce73c4fa79a003626d38056d3ae7c3114
YeonJuan <yeonjuan93@naver.com>
2022-09-29 14:31:41 +0900

    fix: handle CRLF (fixes #110) (#111)

--- packages/eslint-plugin/tests/rules/no-duplicate-attrs.test.js
+++ packages/eslint-plugin/tests/rules/no-duplicate-attrs.test.js
@@ -14,6 +14,20 @@ ruleTester.run("no-duplicate-attrs", rule, {
     {
       code: `<div foo="foo" bar="bar"> </div>`,
     },
+    // https://github.com/yeonjuan/html-eslint/issues/110
+    {
+      code: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+  stroke="currentColor" stroke-width="2" stroke-line-cap="round" stroke-linejoin="round"
+  class="feather feather-home"
+  >
+	<path d="M3 919"></path>
+	<polyline points="9 22"></polyline>
+</svg>
+      `
+        .split("\n")
+        .join("\r\n"),
+    },
   ],
   invalid: [
     {
@@ -29,6 +43,25 @@ ruleTester.run("no-duplicate-attrs", rule, {
       errors: [
         {
           message: "The attribute 'foo' is duplicated.",
+        },
+      ],
+    },
+    // https://github.com/yeonjuan/html-eslint/issues/110
+    {
+      code: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+      class="feather feather-home"
+      class="feather feather-home"
+      >
+      <path d="M3 919"></path>
+      <polyline points="9 22"></polyline>
+    </svg>
+          `
+        .split("\n")
+        .join("\r\n"),
+      errors: [
+        {
+          message: "The attribute 'class' is duplicated.",
         },
       ],
     },
