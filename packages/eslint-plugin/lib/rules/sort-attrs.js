bfe2b6b43d183c425ec0f17da54d8ed6ebf149c3
YeonJuan <yeonjuan93@naver.com>
2023-11-23 19:39:30 +0900

    feat: add sort-attrs (fixes #92) (#162)
    * feat: add sort-attrs (fixes #92)
    
    * docs: fix title

---
+++ packages/eslint-plugin/lib/rules/sort-attrs.js
@@ -0,0 +1,158 @@
+const { RULE_CATEGORY } = require("../constants");
+
+const MESSAGE_IDS = {
+  UNSORTED: "unsorted",
+};
+
+/**
+ * @type {Rule}
+ */
+module.exports = {
+  meta: {
+    type: "code",
+
+    docs: {
+      description: "Enforce attributes alphabetical sorting",
+      category: RULE_CATEGORY.STYLE,
+      recommended: false,
+    },
+    fixable: "code",
+    schema: [
+      {
+        type: "object",
+        properties: {
+          priority: {
+            type: "array",
+            items: {
+              type: "string",
+              uniqueItems: true,
+            },
+          },
+        },
+      },
+    ],
+    messages: {
+      [MESSAGE_IDS.UNSORTED]: "Attributes should be sorted alphabetically",
+    },
+  },
+  create(context) {
+    const sourceCode = context.getSourceCode();
+    const option = context.options[0] || {
+      priority: ["id", "type", "class", "style"],
+    };
+    /**
+     * @type {string[]}
+     */
+    const priority = option.priority;
+
+    /**
+     * @param {AttributeNode} attrA
+     * @param {AttributeNode} attrB
+     * @return {number}
+     */
+    function compare(attrA, attrB) {
+      const keyA = attrA.key.value;
+      const keyB = attrB.key.value;
+      const keyAReservedValue = priority.indexOf(keyA);
+      const keyBReservedValue = priority.indexOf(keyB);
+      if (keyAReservedValue >= 0 && keyBReservedValue >= 0) {
+        return keyAReservedValue - keyBReservedValue;
+      } else if (keyAReservedValue >= 0) {
+        return -1;
+      } else if (keyBReservedValue >= 0) {
+        return 1;
+      }
+      return keyA.localeCompare(keyB);
+    }
+
+    /**
+     * @param {string} source
+     * @param {AttributeNode[]} unsorted
+     * @param {AttributeNode[]} sorted
+     * @returns {string}
+     */
+    function getSortedCode(source, unsorted, sorted) {
+      let result = "";
+      unsorted.forEach((unsortedAttr, index) => {
+        const sortedAttr = sorted[index];
+        result += source.slice(sortedAttr.range[0], sortedAttr.range[1]);
+
+        const nextUnsortedAttr = unsorted[index + 1];
+        if (nextUnsortedAttr) {
+          result += source.slice(
+            unsortedAttr.range[1],
+            nextUnsortedAttr.range[0]
+          );
+        }
+      });
+      return result;
+    }
+
+    /**
+     * @param {RuleFixer} fixer
+     * @param {AttributeNode[]} unsorted
+     * @param {AttributeNode[]} sorted
+     */
+    function fix(fixer, unsorted, sorted) {
+      const source = sourceCode.getText();
+      return fixer.replaceTextRange(
+        [unsorted[0].range[0], unsorted[unsorted.length - 1].range[1]],
+        getSortedCode(source, unsorted, sorted)
+      );
+    }
+
+    /**
+     * @param {AttributeNode[]} before
+     * @param {AttributeNode[]} after
+     * @returns {boolean}
+     */
+    function isChanged(before, after) {
+      for (let i = 0; i < before.length; i++) {
+        if (before[i] !== after[i]) return true;
+      }
+      return false;
+    }
+
+    /**
+     * @param {AttributeNode[]} unsorted
+     */
+    function checkSorting(unsorted) {
+      if (unsorted.length <= 1) {
+        return;
+      }
+
+      const sorted = [...unsorted].sort(compare);
+
+      if (!isChanged(unsorted, sorted)) {
+        return;
+      }
+      const first = unsorted[0];
+      const last = unsorted[unsorted.length - 1];
+      context.report({
+        node: {
+          range: [first.range[0], last.range[1]],
+          loc: {
+            start: first.loc.start,
+            end: last.loc.end,
+          },
+        },
+        messageId: MESSAGE_IDS.UNSORTED,
+        fix(fixer) {
+          return fix(fixer, unsorted, sorted);
+        },
+      });
+    }
+
+    return {
+      ScriptTag(node) {
+        checkSorting(node.attributes);
+      },
+      Tag(node) {
+        checkSorting(node.attributes);
+      },
+      StyleTag(node) {
+        checkSorting(node.attributes);
+      },
+    };
+  },
+};
---
+++ packages/eslint-plugin/tests/rules/sort-attrs.test.js
@@ -0,0 +1,204 @@
+const createRuleTester = require("../rule-tester");
+const rule = require("../../lib/rules/sort-attrs");
+
+const ruleTester = createRuleTester();
+
+ruleTester.run("sort-attrs", rule, {
+  valid: [
+    {
+      code: '<div a="1"></div>',
+    },
+    {
+      code: '<input autocomplete="foo" checked />',
+    },
+    {
+      code: '<input id="foo" type="checkbox" autocomplete="bar" checked />',
+    },
+    {
+      code: "<script type='module' src='https://scripts.js'></script>",
+    },
+    {
+      code: '<style type="text/css" media="all and (max-width: 500px)"',
+    },
+  ],
+  invalid: [
+    {
+      code: "<script src='https://scripts.js' type='module'></script>",
+      output: "<script type='module' src='https://scripts.js'></script>",
+      errors: [{ messageId: "unsorted" }],
+    },
+    {
+      code: '<style media="all and (max-width: 500px)" type="text/css"></style>',
+      output:
+        '<style type="text/css" media="all and (max-width: 500px)"></style>',
+      errors: [{ messageId: "unsorted" }],
+    },
+    {
+      code: '<input autocomplete="bar" checked id="foo" type="checkbox" />',
+      output: '<input id="foo" type="checkbox" autocomplete="bar" checked />',
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: '<input checked autocomplete="foo" />',
+      output: '<input autocomplete="foo" checked />',
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: '<div b="2" a="1"></div>',
+      output: '<div a="1" b="2"></div>',
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: '<div b="2" a="1" c="3"></div>',
+      output: '<div a="1" b="2" c="3"></div>',
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: `
+        <div
+          b="2"
+          a="1"
+          c="3"
+        >
+        </div>
+      `,
+      output: `
+        <div
+          a="1"
+          b="2"
+          c="3"
+        >
+        </div>
+      `,
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: `
+        <div
+          b
+          a="1"
+          c
+        >
+        </div>
+      `,
+      output: `
+        <div
+          a="1"
+          b
+          c
+        >
+        </div>
+      `,
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: `
+        <div
+          b
+               a="1"
+            c
+        >
+        </div>
+      `,
+      output: `
+        <div
+          a="1"
+               b
+            c
+        >
+        </div>
+      `,
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: `
+        <div
+          b
+          id="foo"
+          a="1"
+          c
+        >
+        </div>
+      `,
+      output: `
+        <div
+          id="foo"
+          a="1"
+          b
+          c
+        >
+        </div>
+      `,
+      options: [
+        {
+          priority: ["id"],
+        },
+      ],
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+    {
+      code: `
+        <div
+          style="background:red"
+          b
+          id="foo"
+          a="1"
+          c
+        >
+        </div>
+      `,
+      output: `
+        <div
+          id="foo"
+          style="background:red"
+          a="1"
+          b
+          c
+        >
+        </div>
+      `,
+      options: [
+        {
+          priority: ["id", "style"],
+        },
+      ],
+      errors: [
+        {
+          messageId: "unsorted",
+        },
+      ],
+    },
+  ],
+});
