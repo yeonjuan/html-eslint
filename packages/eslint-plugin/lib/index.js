20b1aad84d309b2ee3e921d11741f6dcf84ba880
yeonjuan <yeonjuan93@naver.com>
2024-01-23 23:40:59 +0900

    chore: add exported plugin types

--- packages/eslint-plugin/lib/index.js
+++ packages/eslint-plugin/lib/index.js
@@ -2,7 +2,11 @@ const rules = require("./rules");
 const recommended = require("./configs/recommended");
 const parser = require("@html-eslint/parser");
 
+/**
+ * @type {{configs: {recommended: typeof recommended,"flat/recommended": import("eslint").Linter.FlatConfig , rules: typeof rules}}}
+ */
 const plugin = {
+  // @ts-ignore
   configs: {
     recommended,
   },
