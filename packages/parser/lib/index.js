b608c7008c665cf8b77addc23a3d4151a5f9e178
YeonJuan <yeonjuan93@naver.com>
2024-04-07 01:48:36 +0900

    fix: export parser meta (#185)
    * fix: export parser meta
    
    * fix

--- packages/parser/lib/index.js
+++ packages/parser/lib/index.js
@@ -1,5 +1,12 @@
 const { parseForESLint } = require("./parser");
+const meta = require("./meta");
 const { NODE_TYPES } = require("./node-types");
 
 module.exports.parseForESLint = parseForESLint;
 module.exports.NODE_TYPES = NODE_TYPES;
+
+module.exports = {
+  parseForESLint,
+  NODE_TYPES,
+  meta,
+};
