06769bfffbcd9a777a078ac4168afb546f56a08e
YeonJuan <yeonjuan93@naver.com>
2024-11-24 17:08:27 +0900

    feat: support linting html in template literals in no-abstract-roles

--- packages/eslint-plugin/tests/rules/no-abstract-roles.test.js
+++ packages/eslint-plugin/tests/rules/no-abstract-roles.test.js
@@ -2,8 +2,9 @@ const createRuleTester = require("../rule-tester");
 const rule = require("../../lib/rules/no-abstract-roles");
 
 const ruleTester = createRuleTester();
+const templateRuleTester = createRuleTester("espree");
 
-ruleTester.run("id-naming-convention", rule, {
+ruleTester.run("no-abstract-roles", rule, {
   valid: [
     {
       code: `<div role="any"> </div>`,
@@ -12,6 +13,24 @@ ruleTester.run("id-naming-convention", rule, {
   invalid: [
     {
       code: `<img role="command">`,
+      errors: [
+        {
+          message: "Unexpected use of an abstract role.",
+        },
+      ],
+    },
+  ],
+});
+
+templateRuleTester.run("[template] no-abstract-roles", rule, {
+  valid: [
+    {
+      code: `html\`<div role="any"> </div>\`;`,
+    },
+  ],
+  invalid: [
+    {
+      code: `html\`<img role="command">\`;`,
       errors: [
         {
           message: "Unexpected use of an abstract role.",
