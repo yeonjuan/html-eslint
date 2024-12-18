60f20c18a0f6faa7881e4639d6ab5e91599a8bb2
YeonJuan <yeonjuan93@naver.com>
2024-12-18 12:36:20 +0900

    add docs

--- packages/eslint-plugin/tests/rules/require-input-label.test.js
+++ packages/eslint-plugin/tests/rules/require-input-label.test.js
@@ -6,24 +6,27 @@ const ruleTester = createRuleTester();
 ruleTester.run("require-input-label", rule, {
   valid: [
     {
-      code: `<input id="foo"></input>`,
+      code: `<input id="foo">`,
     },
     {
-      code: `<textarea id="foo"></input>`,
+      code: `<textarea id="foo"></textarea>`,
     },
     {
-      code: `<input type="hidden"></input>`,
+      code: `<input type="hidden">`,
     },
     {
-      code: `<label>name: <input></input></label>`,
+      code: `<label>name: <input></label>`,
     },
     {
       code: `<textarea aria-labelledby="foo" />`,
     },
+    {
+      code: `<textarea aria-label="foo" />`,
+    },
   ],
   invalid: [
     {
-      code: `<input></input>`,
+      code: `<input type="text">`,
       errors: [
         {
           messageId: "missingLabel",
@@ -31,7 +34,15 @@ ruleTester.run("require-input-label", rule, {
       ],
     },
     {
-      code: `<label>name: </label><input></input>`,
+      code: `<textarea></textarea>`,
+      errors: [
+        {
+          messageId: "missingLabel",
+        },
+      ],
+    },
+    {
+      code: `<label>name: </label><input type="text">`,
       errors: [
         {
           messageId: "missingLabel",
