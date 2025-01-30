03c342fab72635cb9e2f2d549cafaf23988cb125
YeonJuan <yeonjuan93@naver.com>
2025-01-30 20:35:25 +0900

    add integration test

--- packages/intergration-test/fixtures/eslint-v8-legacy-config/.eslintrc.js
+++ packages/intergration-test/fixtures/eslint-v8-legacy-config/.eslintrc.js
@@ -1,3 +1,4 @@
+const { TEMPLATE_ENGINE_SYNTAX } = require("@html-eslint/parser");
 module.exports = {
     overrides: [
         {
@@ -7,15 +8,19 @@ module.exports = {
                 ecmaVersion: 6
             },
             rules: {
-                "@html-eslint/indent": ["error", 2]
+                "@html-eslint/indent": ["error", 2],
             }
         },
         {
             files: ["**/*.html"],
             parser: "@html-eslint/parser",
+            parserOptions: {
+                templateEngineSyntax: TEMPLATE_ENGINE_SYNTAX.HANDLEBAR
+            },
             plugins: ["@html-eslint"],
             rules: {
-                "@html-eslint/indent": ["error", 2]
+                "@html-eslint/indent": ["error", 2],
+                "@html-eslint/sort-attrs": ["error"]
             }
         }
     ]
--- packages/intergration-test/lib/test-utils.js
+++ packages/intergration-test/lib/test-utils.js
@@ -11,11 +11,12 @@ const tmpFile = promisify(tmp.file);
 
 const execFile = promisify(childProcess.execFile);
 const readFile = promisify(fs.readFile);
-
+tmp.setGracefulCleanup();
 /**
  * @param {Object} config
  * @param {string} config.fixtureName
  * @param {string} config.glob
+ * @param {Record<string, string} config.devDependencies
  */
 async function runESLint(config) {
   const testDir = await tmpDir();
@@ -23,6 +24,16 @@ async function runESLint(config) {
     path.join(__dirname, "../fixtures/", config.fixtureName),
     testDir
   );
+  const packageJsonPath = path.join(testDir, "package.json");
+  const packageJson = JSON.parse(
+    fs.readFileSync(packageJsonPath, { encoding: "utf-8" })
+  );
+  packageJson.devDependencies = {
+    ...packageJson.devDependencies,
+    ...config.devDependencies,
+  };
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
   await execFile("yarn", ["install", "--no-immutable"], {
     cwd: testDir,
   });
@@ -33,12 +44,47 @@ async function runESLint(config) {
     {
       cwd: testDir,
     }
-  ).catch(() => null);
+  ).catch(() => {});
 
   const result = await readFile(outFile, "utf-8");
   return JSON.parse(result);
 }
 
+/**
+ * @returns {Record<string, string>}
+ */
+async function packPackages() {
+  const tarFolder = tmp.dirSync({
+    keep: true,
+  }).name;
+
+  const PACKAGES_DIR = path.resolve(__dirname, "../../");
+  const PACKAGES = fs.readdirSync(PACKAGES_DIR);
+  const dependencies = {};
+  for (const pkg of PACKAGES) {
+    const packageDir = path.join(PACKAGES_DIR, pkg);
+    const packageJsonPath = path.join(packageDir, "package.json");
+    if (!fs.existsSync(packageJsonPath)) {
+      continue;
+    }
+    const packageJson = JSON.parse(
+      fs.readFileSync(packageJsonPath, { encoding: "utf-8" })
+    );
+    if (packageJson.private === true) {
+      continue;
+    }
+    const result = childProcess.spawnSync("npm", ["pack", packageDir], {
+      cwd: tarFolder,
+      encoding: "utf-8",
+    });
+    const stdoutLines = result.stdout.trim().split("\n");
+    const tarball = stdoutLines[stdoutLines.length - 1];
+    dependencies[packageJson.name] = `file:${path.join(tarFolder, tarball)}`;
+  }
+  return dependencies;
+}
+
 module.exports = {
   runESLint,
+  packPackages,
 };
