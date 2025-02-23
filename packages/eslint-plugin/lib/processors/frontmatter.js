e08f5ec62a33a1a60eeb8157214f3a44bbd0e343
YeonJuan <yeonjuan93@naver.com>
2025-02-23 19:43:10 +0900

    feat: add frontmatter processors

---
+++ packages/eslint-plugin/lib/processors/frontmatter.js
@@ -0,0 +1,73 @@
+/**
+ * @typedef { import('eslint').Linter.Processor } Processor
+ */
+
+/**
+ * @param {string} text
+ * @returns {boolean}
+ */
+function hasOpenFrontmatter(text) {
+  return /^---[\r|\n|\r\n]/.test(text);
+}
+
+/**
+ * @param {string} text
+ * @returns {number | null}
+ */
+function indexOfCloseFrontmatter(text) {
+  const endExpExec = /^---[\r|\n|\r\n]/m.exec(text.slice(3));
+  if (!endExpExec) {
+    return null;
+  }
+  const end = endExpExec.index + endExpExec[0].length;
+  return end + 3;
+}
+
+/**
+ * @param {string} text
+ * @returns {number}
+ */
+function countLines(text) {
+  return text.split(/[\r|\n|\r\n]/).length;
+}
+
+/**
+ * @type {number | null}
+ */
+let frontmatterCloseIndex = null;
+let frontmatterLines = 0;
+
+/**
+ * @type {Processor}
+ */
+const frontmatter = {
+  supportsAutofix: false,
+  preprocess(text) {
+    frontmatterCloseIndex = null;
+
+    if (hasOpenFrontmatter(text)) {
+      frontmatterCloseIndex = indexOfCloseFrontmatter(text);
+
+      if (typeof frontmatterCloseIndex === "number") {
+        frontmatterLines = countLines(text.slice(0, frontmatterCloseIndex));
+        return [text.slice(frontmatterCloseIndex)];
+      }
+    }
+    return [text];
+  },
+  postprocess(messagesArray) {
+    const messages = messagesArray[0];
+
+    if (typeof frontmatterCloseIndex === "number") {
+      for (const message of messages) {
+        message.line += frontmatterLines;
+        if ("endLine" in message && typeof message.endLine === "number") {
+          message.endLine += frontmatterLines;
+        }
+      }
+    }
+    return messages;
+  },
+};
+
+module.exports = frontmatter;
