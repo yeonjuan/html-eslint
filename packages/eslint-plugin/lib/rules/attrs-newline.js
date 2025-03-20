12a1a801c1e52909a89c70e5be45710325648050
MorevM <max.seainside@gmail.com>
2025-03-21 01:18:09 +0300

    fix(attrs-newline): Report only if number of attributes in one line is greater than `ifAttrsMoreThan`, remove reverse validation

--- packages/eslint-plugin/lib/rules/attrs-newline.js
+++ packages/eslint-plugin/lib/rules/attrs-newline.js
@@ -4,7 +4,6 @@
  * @typedef {Object} MessageId
  * @property {"closeStyleWrong"} CLOSE_STYLE_WRONG
  * @property {"newlineMissing"} NEWLINE_MISSING
- * @property {"newlineUnexpected"} NEWLINE_UNEXPECTED
  *
  * @typedef {Object} Option
  * @property {"sameline" | "newline"} [option.closeStyle]
@@ -22,7 +21,6 @@ const { createVisitors } = require("./utils/visitors");
 const MESSAGE_ID = {
   CLOSE_STYLE_WRONG: "closeStyleWrong",
   NEWLINE_MISSING: "newlineMissing",
-  NEWLINE_UNEXPECTED: "newlineUnexpected",
 };
 
 /**
@@ -56,8 +54,6 @@ module.exports = {
       [MESSAGE_ID.CLOSE_STYLE_WRONG]:
         "Closing bracket was on {{actual}}; expected {{expected}}",
       [MESSAGE_ID.NEWLINE_MISSING]: "Newline expected before {{attrName}}",
-      [MESSAGE_ID.NEWLINE_UNEXPECTED]:
-        "Newlines not expected between attributes, since this tag has fewer than {{attrMin}} attributes",
     },
   },
 
@@ -70,21 +66,22 @@ module.exports = {
     return createVisitors(context, {
       Tag(node) {
         const shouldBeMultiline = node.attributes.length > attrMin;
+        if (!shouldBeMultiline) return;
 
         /**
          * This doesn't do any indentation, so the result will look silly. Indentation should be covered by the `indent` rule
          * @param {RuleFixer} fixer
          */
         function fix(fixer) {
-          const spacer = shouldBeMultiline ? "\n" : " ";
           let expected = node.openStart.value;
           for (const attr of node.attributes) {
-            expected += `${spacer}${attr.key.value}`;
+            expected += `\n${attr.key.value}`;
             if (attr.startWrapper && attr.value && attr.endWrapper) {
               expected += `=${attr.startWrapper.value}${attr.value.value}${attr.endWrapper.value}`;
             }
           }
-          if (shouldBeMultiline && closeStyle === "newline") {
+
+          if (closeStyle === "newline") {
             expected += "\n";
           } else if (node.selfClosing) {
             expected += " ";
@@ -97,66 +94,38 @@ module.exports = {
           );
         }
 
-        if (shouldBeMultiline) {
-          let index = 0;
-          for (const attr of node.attributes) {
-            const attrPrevious = node.attributes[index - 1];
-            const relativeToNode = attrPrevious || node.openStart;
-            if (attr.loc.start.line === relativeToNode.loc.end.line) {
-              return context.report({
-                node,
-                data: {
-                  attrName: attr.key.value,
-                },
-                fix,
-                messageId: MESSAGE_ID.NEWLINE_MISSING,
-              });
-            }
-            index += 1;
-          }
-
-          const attrLast = node.attributes[node.attributes.length - 1];
-          const closeStyleActual =
-            node.openEnd.loc.start.line === attrLast.loc.end.line
-              ? "sameline"
-              : "newline";
-          if (closeStyle !== closeStyleActual) {
+        let index = 0;
+        for (const attr of node.attributes) {
+          const attrPrevious = node.attributes[index - 1];
+          const relativeToNode = attrPrevious || node.openStart;
+          if (attr.loc.start.line === relativeToNode.loc.end.line) {
             return context.report({
               node,
               data: {
-                actual: closeStyleActual,
-                expected: closeStyle,
+                attrName: attr.key.value,
               },
               fix,
-              messageId: MESSAGE_ID.CLOSE_STYLE_WRONG,
+              messageId: MESSAGE_ID.NEWLINE_MISSING,
             });
           }
-        } else {
-          let expectedLastLineNum = node.openStart.loc.start.line;
-          for (const attr of node.attributes) {
-            if (shouldBeMultiline) {
-              expectedLastLineNum += 1;
-            }
-            if (attr.value) {
-              const valueLineSpan =
-                attr.value.loc.end.line - attr.value.loc.start.line;
-              expectedLastLineNum += valueLineSpan;
-            }
-          }
-          if (shouldBeMultiline && closeStyle === "newline") {
-            expectedLastLineNum += 1;
-          }
+          index += 1;
+        }
 
-          if (node.openEnd.loc.end.line !== expectedLastLineNum) {
-            return context.report({
-              node,
-              data: {
-                attrMin: `${attrMin}`,
-              },
-              fix,
-              messageId: MESSAGE_ID.NEWLINE_UNEXPECTED,
-            });
-          }
+        const attrLast = node.attributes[node.attributes.length - 1];
+        const closeStyleActual =
+          node.openEnd.loc.start.line === attrLast.loc.end.line
+            ? "sameline"
+            : "newline";
+        if (closeStyle !== closeStyleActual) {
+          return context.report({
+            node,
+            data: {
+              actual: closeStyleActual,
+              expected: closeStyle,
+            },
+            fix,
+            messageId: MESSAGE_ID.CLOSE_STYLE_WRONG,
+          });
         }
       },
     });
