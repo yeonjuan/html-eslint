00109524f3f6121800ab3fc6de4cd122b0bf0df7
YeonJuan <yeonjuan93@naver.com>
2025-03-25 20:55:49 +0900

    feat: add fix and suggest in require-button-type (#305)
    * feat: add fix and suggest in require-button-type
    
    * Update require-button-type.js
    
    * fixup

--- packages/eslint-plugin/lib/rules/require-button-type.js
+++ packages/eslint-plugin/lib/rules/require-button-type.js
@@ -1,5 +1,7 @@
 /**
  * @typedef { import("../types").RuleModule<[]> } RuleModule
+ * @typedef { import("@html-eslint/types").AttributeValue } AttributeValue
+ * @typedef { import("../types").SuggestionReportDescriptor } SuggestionReportDescriptor
  */
 
 const { RULE_CATEGORY } = require("../constants");
@@ -9,6 +11,9 @@ const { createVisitors } = require("./utils/visitors");
 const MESSAGE_IDS = {
   MISSING: "missing",
   INVALID: "invalid",
+  REPLACE_TO_SUBMIT: "replaceToSubmit",
+  REPLACE_TO_BUTTON: "replaceToButton",
+  REPLACE_TO_RESET: "replaceToReset",
 };
 
 const VALID_BUTTON_TYPES_SET = new Set(["submit", "button", "reset"]);
@@ -26,16 +31,41 @@ module.exports = {
       recommended: false,
     },
 
-    fixable: null,
+    fixable: true,
+    hasSuggestions: true,
     schema: [],
     messages: {
       [MESSAGE_IDS.MISSING]: "Missing a type attribute for button",
       [MESSAGE_IDS.INVALID]:
         '"{{type}}" is an invalid value for button type attribute.',
+      [MESSAGE_IDS.REPLACE_TO_BUTTON]: "Replace the type with 'button'",
+      [MESSAGE_IDS.REPLACE_TO_SUBMIT]: "Replace the type with 'submit'",
+      [MESSAGE_IDS.REPLACE_TO_RESET]: "Replace the type with 'reset'",
     },
   },
 
   create(context) {
+    /**
+     * @param {AttributeValue} node
+     * @returns {SuggestionReportDescriptor[]}
+     */
+    function getSuggestions(node) {
+      return [
+        {
+          messageId: MESSAGE_IDS.REPLACE_TO_SUBMIT,
+          fix: (fixer) => fixer.replaceTextRange(node.range, "submit"),
+        },
+        {
+          messageId: MESSAGE_IDS.REPLACE_TO_BUTTON,
+          fix: (fixer) => fixer.replaceTextRange(node.range, "button"),
+        },
+        {
+          messageId: MESSAGE_IDS.REPLACE_TO_RESET,
+          fix: (fixer) => fixer.replaceTextRange(node.range, "reset"),
+        },
+      ];
+    }
+
     return createVisitors(context, {
       Tag(node) {
         if (node.name !== "button") {
@@ -46,6 +76,9 @@ module.exports = {
           context.report({
             node: node.openStart,
             messageId: MESSAGE_IDS.MISSING,
+            fix(fixer) {
+              return fixer.insertTextAfter(node.openStart, ' type="submit"');
+            },
           });
         } else if (
           !VALID_BUTTON_TYPES_SET.has(typeAttr.value.value) &&
@@ -57,6 +90,7 @@ module.exports = {
             data: {
               type: typeAttr.value.value,
             },
+            suggest: getSuggestions(typeAttr.value),
           });
         }
       },
