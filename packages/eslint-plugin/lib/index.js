a78e99ec101545eac50309b5844d40b7984f2621
YeonJuan <yeonjuan93@naver.com>
2025-05-11 21:04:23 +0900

    Update index.js

--- packages/eslint-plugin/lib/index.js
+++ packages/eslint-plugin/lib/index.js
@@ -7,12 +7,12 @@ const { name, version } = require("../package.json");
  * @typedef {import("./rules")} AllRules
  * @typedef {import("./configs/recommended")} RecommendedConfig
  * @typedef {{name: string, version: string}} PluginMeta
- * @typedef {{recommended: RecommendedConfig, "flat/recommended": import("eslint").Linter.FlatConfig }} HTMLESLintConfigs
+ * @typedef {{recommended: RecommendedConfig, "flat/recommended": import("eslint").Linter.FlatConfig }} HtmlESLintConfigs
  * @typedef {{html: HTMLLanguage}} Languages
  */
 
 /**
- * @type {{meta: PluginMeta, rules: AllRules, configs: HTMLESLintConfigs, languages: Languages}}
+ * @type {{meta: PluginMeta, rules: AllRules, configs: HtmlESLintConfigs, languages: Languages}}
  */
 const plugin = {
   meta: {
