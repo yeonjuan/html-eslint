4f995f497bb90bd222d2c73ea66ea7b74d512ab9
YeonJuan <yeonjuan93@naver.com>
2025-11-11 23:59:02 +0900

    refactor: use plugin-kit native method

--- packages/eslint-plugin/tests/languages/html-source-code.test.js
+++ packages/eslint-plugin/tests/languages/html-source-code.test.js
@@ -3,7 +3,9 @@
  */
 
 const { HTMLLanguage } = require("../../lib/languages/html-language");
-const { HTMLSourceCode } = require("../../lib/languages/html-source-code");
+const {
+  createHTMLSourceCode,
+} = require("../../lib/languages/html-source-code");
 
 /**
  * @param {string} text
@@ -25,7 +27,7 @@ const createSourceCode = (text) => {
   const language = new HTMLLanguage();
   const file = createFile(text);
   const parsed = language.parse(file);
-  const sourceCode = new HTMLSourceCode({
+  const sourceCode = createHTMLSourceCode({
     text,
     // @ts-ignore
     ast: parsed.ast,
