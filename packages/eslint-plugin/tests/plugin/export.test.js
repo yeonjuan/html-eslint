ee2e6433273bba2275e5e38ceec280b3451fe677
YeonJuan <yeonjuan93@naver.com>
2025-11-20 23:01:02 +0900

    Update export.test.js

--- packages/eslint-plugin/tests/plugin/export.test.js
+++ packages/eslint-plugin/tests/plugin/export.test.js
@@ -1,7 +1,9 @@
 const plugin = require("../../lib");
 describe("@html-eslint/eslint-plugin", () => {
   it("should have meta", () => {
+    // @ts-ignore
     expect(plugin.meta.name).toBe("@html-eslint/eslint-plugin");
+    // @ts-ignore
     expect(typeof plugin.meta.version).toBe("string");
   });
 });
