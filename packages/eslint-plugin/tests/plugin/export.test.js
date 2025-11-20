e7568dafb9c0d31ca62f905bec45d812ee393d0d
YeonJuan <yeonjuan93@naver.com>
2025-11-20 23:10:07 +0900

    Update export.test.js

--- packages/eslint-plugin/tests/plugin/export.test.js
+++ packages/eslint-plugin/tests/plugin/export.test.js
@@ -1,9 +1,20 @@
 const plugin = require("../../lib");
+
+/**
+ * @template T
+ * @param {T | null | undefined} value
+ * @returns {asserts value is NonNullable<T>}
+ */
+function assertNonNull(value) {
+  if (value === undefined || value === null) {
+    throw new TypeError("Unexpected nullish");
+  }
+}
+
 describe("@html-eslint/eslint-plugin", () => {
   it("should have meta", () => {
-    // @ts-ignore
+    assertNonNull(plugin.meta);
     expect(plugin.meta.name).toBe("@html-eslint/eslint-plugin");
-    // @ts-ignore
     expect(typeof plugin.meta.version).toBe("string");
   });
 });
