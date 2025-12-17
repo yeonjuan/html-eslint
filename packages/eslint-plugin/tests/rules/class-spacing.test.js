9203e113f522c19eac588260281f4f22f77e8679
YeonJuan <yeonjuan93@naver.com>
2025-12-17 22:15:54 +0900

    feat: add class-spacing rule (#465)
    * feat: add class-spacing rule
    
    * fix format

---
+++ packages/eslint-plugin/tests/rules/class-spacing.test.js
@@ -0,0 +1,157 @@
+const createRuleTester = require("../rule-tester");
+const rule = require("../../lib/rules/class-spacing");
+
+const ruleTester = createRuleTester();
+const templateRuleTester = createRuleTester("espree");
+
+ruleTester.run("class-spacing", rule, {
+  valid: [
+    {
+      code: `<button class="foo"></button>`,
+    },
+    {
+      code: `<button class="foo bar"></button>`,
+    },
+    {
+      code: `<button class="foo bar baz"></button>`,
+    },
+    {
+      code: `<button class=""></button>`,
+    },
+    {
+      code: `<button class></button>`,
+    },
+    {
+      code: `<button id=" foo "></button>`,
+    },
+  ],
+  invalid: [
+    {
+      code: `<button class=" foo"></button>`,
+      output: `<button class="foo"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingStart",
+        },
+      ],
+    },
+    {
+      code: `<button class="foo "></button>`,
+      output: `<button class="foo"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingEnd",
+        },
+      ],
+    },
+    {
+      code: `<button class=" foo "></button>`,
+      output: `<button class="foo"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingStart",
+        },
+      ],
+    },
+    {
+      code: `<button class="  foo"></button>`,
+      output: `<button class="foo"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingStart",
+        },
+      ],
+    },
+    {
+      code: `<button class="foo  "></button>`,
+      output: `<button class="foo"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingEnd",
+        },
+      ],
+    },
+    {
+      code: `<button class="foo  bar"></button>`,
+      output: `<button class="foo bar"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingBetween",
+        },
+      ],
+    },
+    {
+      code: `<button class="foo   bar"></button>`,
+      output: `<button class="foo bar"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingBetween",
+        },
+      ],
+    },
+    {
+      code: `<button class="foo  bar  baz"></button>`,
+      output: `<button class="foo bar baz"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingBetween",
+        },
+      ],
+    },
+    {
+      code: `<button class="  foo  bar  "></button>`,
+      output: `<button class="foo bar"></button>`,
+      errors: [
+        {
+          messageId: "extraSpacingStart",
+        },
+      ],
+    },
+  ],
+});
+
+templateRuleTester.run("[template] class-spacing", rule, {
+  valid: [
+    {
+      code: "html`<div class='foo'></div>`",
+    },
+    {
+      code: "html`<div class='foo bar'></div>`",
+    },
+    {
+      code: "html`<div class=${' foo  bar '}></div>`",
+    },
+    {
+      code: "html`<div class='${' foo  bar '} baz'></div>`",
+    },
+  ],
+  invalid: [
+    {
+      code: `html\`<div class=' foo'></div>\``,
+      output: `html\`<div class='foo'></div>\``,
+      errors: [
+        {
+          messageId: "extraSpacingStart",
+        },
+      ],
+    },
+    {
+      code: `html\`<div class='foo '></div>\``,
+      output: `html\`<div class='foo'></div>\``,
+      errors: [
+        {
+          messageId: "extraSpacingEnd",
+        },
+      ],
+    },
+    {
+      code: `html\`<div class='foo  bar'></div>\``,
+      output: `html\`<div class='foo bar'></div>\``,
+      errors: [
+        {
+          messageId: "extraSpacingBetween",
+        },
+      ],
+    },
+  ],
+});
