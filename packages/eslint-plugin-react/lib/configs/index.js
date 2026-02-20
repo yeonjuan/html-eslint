4061671e44afa4e2fddd95adf5835814f018d2f0
YeonJuan <yeonjuan93@naver.com>
2026-02-20 21:27:19 +0900

    Update index.js

--- packages/eslint-plugin-react/lib/configs/index.js
+++ packages/eslint-plugin-react/lib/configs/index.js
@@ -3,8 +3,11 @@ const ruleKeys = Object.keys(rules);
 
 const allRules = ruleKeys.reduce((acc, ruleName) => {
   acc[`@html-eslint/react/${ruleName}`] = "error";
+  if (ruleName === "use-baseline") {
+    acc[`@html-eslint/react/${ruleName}`] = "warning";
+  }
   return acc;
-}, /** @type {Record<string, "error">} */ ({}));
+}, /** @type {Record<string, "error" | "warning">} */ ({}));
 
 module.exports = {
   allRules,
