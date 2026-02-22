fbd36fbc5665b9b1bb218e5c7d8407e465798c3f
YeonJuan <yeonjuan93@naver.com>
2026-02-22 19:11:58 +0900

    refactor

--- packages/integration-test/tests/integration.test.js
+++ packages/integration-test/tests/integration.test.js
@@ -1,285 +1,138 @@
 const { runESLint, runTypecheck } = require("../lib/test-utils");
 
+/** Helper function to test ESLint results */
+function expectLintResult(result, { shouldHaveErrors = false } = {}) {
+  expect(result[0].fatalErrorCount).toBe(0);
+  if (shouldHaveErrors) {
+    expect(result[0].messages.length).toBeGreaterThanOrEqual(1);
+  } else {
+    expect(result[0].messages.length).toBe(0);
+  }
+}
+
+/** Helper function to test multiple files with ESLint */
+async function testESLintFiles({
+  fixtureName,
+  eslintVersion,
+  localPackages,
+  files,
+  shouldHaveErrors = false,
+}) {
+  for (const file of files) {
+    const result = await runESLint({
+      fixtureName,
+      eslintVersion,
+      localPackages,
+      glob: file,
+      log: !shouldHaveErrors,
+    });
+    expectLintResult(result, { shouldHaveErrors });
+  }
+}
+
+/** Create standard test suite for ESLint configuration */
+function createESLintConfigTests({
+  eslintVersion,
+  fixtureName,
+  localPackages,
+  invalidFiles = [
+    "html/invalid.html",
+    "js/invalid.js",
+    "frontmatter/invalid.html",
+  ],
+  validFiles = ["html/valid.html", "js/valid.js", "frontmatter/valid.html"],
+  testTimeout = 20000,
+}) {
+  const config = { eslintVersion, fixtureName, localPackages };
+
+  if (invalidFiles.length > 0) {
+    it(
+      "should throw a lint error for invalid files",
+      async () => {
+        await testESLintFiles({
+          ...config,
+          files: invalidFiles,
+          shouldHaveErrors: true,
+        });
+      },
+      testTimeout
+    );
+  }
+
+  if (validFiles.length > 0) {
+    it(
+      "should not throw any lint error for valid files",
+      async () => {
+        await testESLintFiles({
+          ...config,
+          files: validFiles,
+          shouldHaveErrors: false,
+        });
+      },
+      testTimeout
+    );
+  }
+}
+
 describe("integration tests", () => {
   describe("eslint-v8-legacy-config", () => {
-    const eslintVersion = "8";
-    const fixtureName = "eslint-v8-legacy-config";
-    const localPackages = ["@html-eslint/eslint-plugin", "@html-eslint/parser"];
-
-    it("should throw a lint error for invalid files", async () => {
-      const htmlResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "html/invalid.html",
-        log: false,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const jsResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "js/invalid.js",
-        log: false,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const frontmatterResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "frontmatter/invalid.html",
-        log: false,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBeGreaterThanOrEqual(1);
-    }, 20000);
-
-    it("should not throw any lint error for valid files", async () => {
-      const htmlResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "html/valid.html",
-        log: true,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBe(0);
-
-      const jsResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "js/valid.js",
-        log: true,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBe(0);
-
-      const frontmatterResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "frontmatter/valid.html",
-        log: true,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBe(0);
-    }, 20000);
+    createESLintConfigTests({
+      eslintVersion: "8",
+      fixtureName: "eslint-v8-legacy-config",
+      localPackages: ["@html-eslint/eslint-plugin", "@html-eslint/parser"],
+    });
   });
 
   describe("eslint-v9-flat-config", () => {
-    const eslintVersion = "9";
-    const fixtureName = "eslint-v9-flat-config";
-    const localPackages = ["@html-eslint/eslint-plugin", "@html-eslint/parser"];
-
-    it("should throw a lint error for invalid files", async () => {
-      const htmlResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "html/invalid.html",
-        log: false,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const jsResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "js/invalid.js",
-        log: false,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const frontmatterResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "frontmatter/invalid.html",
-        log: false,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBeGreaterThanOrEqual(1);
-    }, 20000);
-
-    it("should not throw any lint error for valid files", async () => {
-      const htmlResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "html/valid.html",
-        log: true,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBe(0);
-
-      const jsResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "js/valid.js",
-        log: true,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBe(0);
-
-      const frontmatterResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "frontmatter/valid.html",
-        log: true,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBe(0);
-    }, 20000);
+    createESLintConfigTests({
+      eslintVersion: "9",
+      fixtureName: "eslint-v9-flat-config",
+      localPackages: ["@html-eslint/eslint-plugin", "@html-eslint/parser"],
+    });
   });
 
   describe("eslint-v9-language", () => {
-    const eslintVersion = "9";
-    const fixtureName = "eslint-v9-language";
-    const localPackages = ["@html-eslint/eslint-plugin"];
-
-    it("should not throw any lint error for valid files", async () => {
-      const htmlResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "html/valid.html",
-        log: true,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBe(0);
-    }, 20000);
+    createESLintConfigTests({
+      eslintVersion: "9",
+      fixtureName: "eslint-v9-language",
+      localPackages: ["@html-eslint/eslint-plugin"],
+      invalidFiles: [],
+      validFiles: ["html/valid.html"],
+    });
   });
 
   describe("eslint-v10-flat-config", () => {
-    const eslintVersion = "10";
-    const fixtureName = "eslint-v10-flat-config";
-    const localPackages = ["@html-eslint/eslint-plugin", "@html-eslint/parser"];
-
-    it("should throw a lint error for invalid files", async () => {
-      const htmlResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "html/invalid.html",
-        log: false,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const jsResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "js/invalid.js",
-        log: false,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBeGreaterThanOrEqual(1);
-
-      const frontmatterResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "frontmatter/invalid.html",
-        log: false,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBeGreaterThanOrEqual(1);
-    }, 20000);
-
-    it("should not throw any lint error for valid files", async () => {
-      const htmlResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "html/valid.html",
-        log: true,
-      });
-      expect(htmlResult[0].fatalErrorCount).toBe(0);
-      expect(htmlResult[0].messages.length).toBe(0);
-
-      const jsResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "js/valid.js",
-        log: true,
-      });
-      expect(jsResult[0].fatalErrorCount).toBe(0);
-      expect(jsResult[0].messages.length).toBe(0);
-
-      const frontmatterResult = await runESLint({
-        eslintVersion,
-        fixtureName,
-        localPackages,
-        glob: "frontmatter/valid.html",
-        log: true,
-      });
-      expect(frontmatterResult[0].fatalErrorCount).toBe(0);
-      expect(frontmatterResult[0].messages.length).toBeGreaterThanOrEqual(0);
-    }, 20000);
+    createESLintConfigTests({
+      eslintVersion: "10",
+      fixtureName: "eslint-v10-flat-config",
+      localPackages: ["@html-eslint/eslint-plugin", "@html-eslint/parser"],
+    });
   });
 
   describe("typescript", () => {
     const eslintVersion = "9.27.0";
     const fixtureName = "typescript";
     const localPackages = ["@html-eslint/eslint-plugin"];
-    it("should not throw any type error", async () => {
-      const result927 = await runTypecheck({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        fileName: "eslint.config.ts",
-        log: true,
-      });
-      expect(result927).toBe(undefined);
 
-      const result9392 = await runTypecheck({
+    it("should not throw any type error", async () => {
+      const result = await runTypecheck({
         fixtureName,
         eslintVersion,
         localPackages,
         fileName: "eslint.config.ts",
         log: true,
       });
-      expect(result9392).toBe(undefined);
+      expect(result).toBe(undefined);
     }, 20000);
   });
 
   describe("react", () => {
-    const fixtureName = "react";
-    const eslintVersion = "9.27.0";
-    const localPackages = ["@html-eslint/eslint-plugin-react"];
-    it("should throw a lint error for invalid files", async () => {
-      const jsxResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "jsx/invalid.jsx",
-        log: false,
-      });
-      expect(jsxResult[0].fatalErrorCount).toBe(0);
-      expect(jsxResult[0].messages.length).toBeGreaterThanOrEqual(1);
-    }, 20000);
-
-    it("should not throw any lint error for valid files", async () => {
-      const jsxResult = await runESLint({
-        fixtureName,
-        eslintVersion,
-        localPackages,
-        glob: "jsx/valid.jsx",
-        log: true,
-      });
-      expect(jsxResult[0].fatalErrorCount).toBe(0);
-      expect(jsxResult[0].messages.length).toBe(0);
-    }, 20000);
+    createESLintConfigTests({
+      eslintVersion: "9.27.0",
+      fixtureName: "react",
+      localPackages: ["@html-eslint/eslint-plugin-react"],
+      invalidFiles: ["jsx/invalid.jsx"],
+      validFiles: ["jsx/valid.jsx"],
+    });
   });
 });
