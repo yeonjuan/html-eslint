194f47f692c1f5dded60a176a4152f0e47d5a1a5
YeonJuan <yeonjuan93@naver.com>
2026-03-03 22:31:21 +0900

    feat: add angular/no-ineffective-attrs

--- packages/eslint-plugin-angular-template/lib/rules/index.js
+++ packages/eslint-plugin-angular-template/lib/rules/index.js
@@ -2,12 +2,14 @@ const useBaseline = require("./use-baseline");
 const noDuplicateClass = require("./no-duplicate-class");
 const classSpacing = require("./class-spacing");
 const noObsoleteTags = require("./no-obsolete-tags");
+const noIneffectiveAttrs = require("./no-ineffective-attrs");
 
 const rules = {
   "use-baseline": useBaseline,
   "no-duplicate-class": noDuplicateClass,
   "class-spacing": classSpacing,
   "no-obsolete-tags": noObsoleteTags,
+  "no-ineffective-attrs": noIneffectiveAttrs,
 };
 
 module.exports = rules;
