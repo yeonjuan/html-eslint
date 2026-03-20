151bcadcf3967ea84b34aac537c77b18ff52270a
Nova Vale <novavale1@hotmail.com>
2026-03-20 09:12:43 -0700

    feat(svg-require-viewbox): add rule to enforce viewBox on svg elements

---
+++ packages/eslint-plugin/lib/rules/svg-require-viewbox.js
@@ -0,0 +1,52 @@
+/**
+ * @import {
+ *   AnyNode,
+ *   Tag
+ * } from "@html-eslint/types"
+ * @import {RuleModule} from "../types"
+ */
+
+const { RULE_CATEGORY } = require("../constants");
+const { findAttr, isTag } = require("./utils/node");
+const { getRuleUrl } = require("./utils/rule");
+
+const MESSAGE_IDS = {
+  MISSING: "missing",
+};
+
+/** @type {RuleModule<[]>} */
+module.exports = {
+  meta: {
+    type: "code",
+
+    docs: {
+      description: "Require `viewBox` attribute on `<svg>` elements",
+      category: RULE_CATEGORY.BEST_PRACTICE,
+      recommended: false,
+      url: getRuleUrl("svg-require-viewbox"),
+    },
+
+    fixable: null,
+    schema: [],
+    messages: {
+      [MESSAGE_IDS.MISSING]: "Missing `viewBox` attribute on `<svg>`.",
+    },
+  },
+
+  create(context) {
+    return {
+      Tag(node) {
+        if (node.name !== "svg") {
+          return;
+        }
+
+        if (!findAttr(node, "viewBox")) {
+          context.report({
+            node,
+            messageId: MESSAGE_IDS.MISSING,
+          });
+        }
+      },
+    };
+  },
+};
