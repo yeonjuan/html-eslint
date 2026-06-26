950b3b504d529bc76f48b0d707de822e82509485
YeonJuan <yeonjuan93@naver.com>
2026-06-26 19:30:35 +0900

    feat: add allowExpressions options on no-inline-styles (#626)
    * feat: add allowExpressions options on no-inline-styles
    
    * Update no-inline-styles.md
    
    * update
    
    * Update no-inline-styles.md

--- packages/eslint-plugin/tests/rules/no-inline-styles.test.js
+++ packages/eslint-plugin/tests/rules/no-inline-styles.test.js
@@ -15,6 +15,28 @@ ruleTester.run("no-inline-styles", rule, {
 </html>
 `,
     },
+    {
+      code: `<div style="{{ styles }}"></div>`,
+      options: [{ allowExpressions: true }],
+      languageOptions: {
+        parserOptions: {
+          templateEngineSyntax: {
+            "{{": "}}",
+          },
+        },
+      },
+    },
+    {
+      code: `<div style="color: red; {{ moreStyles }}"></div>`,
+      options: [{ allowExpressions: true }],
+      languageOptions: {
+        parserOptions: {
+          templateEngineSyntax: {
+            "{{": "}}",
+          },
+        },
+      },
+    },
   ],
   invalid: [
     {
@@ -29,6 +51,25 @@ ruleTester.run("no-inline-styles", rule, {
       errors: [
         {
           messageId: "unexpectedInlineStyle",
+        },
+      ],
+    },
+    {
+      code: `<div style="{{ styles }}"></div>`,
+      languageOptions: {
+        parserOptions: {
+          templateEngineSyntax: {
+            "{{": "}}",
+          },
+        },
+      },
+      errors: [
+        {
+          messageId: "unexpectedInlineStyle",
+          line: 1,
+          column: 6,
+          endLine: 1,
+          endColumn: 26,
         },
       ],
     },
@@ -51,6 +92,19 @@ nohtml\`<div style="color:#fff"></div>\`
       code: `
 const html = \`<div style="color:#fff"></div>\`
       `,
+    },
+    {
+      code: `
+let height = 150;
+html\`<div style="height: \${height}px"></div>\`
+      `,
+      options: [{ allowExpressions: true }],
+    },
+    {
+      code: `
+html\`<div style="\${styles}"></div>\`
+      `,
+      options: [{ allowExpressions: true }],
     },
   ],
   invalid: [
@@ -94,6 +148,21 @@ const html = /* html */\`<div style="color:#fff"></div>\`
           column: 30,
           endLine: 2,
           endColumn: 48,
+        },
+      ],
+    },
+    {
+      code: `
+html\`<div style="color:#fff"></div>\`
+      `,
+      options: [{ allowExpressions: true }],
+      errors: [
+        {
+          messageId: "unexpectedInlineStyle",
+          line: 2,
+          column: 11,
+          endLine: 2,
+          endColumn: 29,
         },
       ],
     },
